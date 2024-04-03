9dc7ba201ca375fb7ddb0a06182713d6dee554a4
Jonathan-Robles <jhsiete@gmail.com>
2024-04-03 12:25:12 -0500

    commit

--- src/main.py
+++ src/main.py
@@ -4,5 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("Table.html")#"<p>Hello, World!</p>"
+def home():
+  return render_template("Table.html")
+    
+@app.route("/about")
+def about():
+  return render_template("about.html")
+  
+if __name__ == '__main__':
+  app.run(debug=True)
