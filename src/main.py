76dfa7257ec5ecf2c9706d73735bc18ca7a32311
Jonathan-Robles <jhsiete@gmail.com>
2024-10-02 08:30:10 -0500

    actualizacion tutoriales

--- src/main.py
+++ src/main.py
@@ -10,6 +10,14 @@ def home():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/entornos_virtuales_python")
+def entornos_virtuales_python():
+  return render_template("entornos_virtuales_python.html")
+
+@app.route("/flask_tutorial")
+def flask_tutorial():
+  return render_template("flask_tutorial.html")
   
 if __name__ == '__main__':
   app.run(debug=True)
